98ccee9677a5c9fd76b3d9ac33e041dd9d031396
RealRedChief <37128377+RealRedChief@users.noreply.github.com>
2018-11-04 01:46:20 -0500

    Stuff

--- public/something.js
+++ public/something.js
@@ -4,25 +4,29 @@ const chat = document.querySelector('#chat');
 const userForm = document.querySelector('.listenhere');
 const username = document.querySelector('#username');
 const status = document.querySelector('.status');
-const form = document.querySelector('form');
-
+const form = document.querySelector('#messageform');
+const statusDefault = status.textContent;
 
 userForm.querySelector('input').addEventListener('input', getUser);
-userForm.addEventListener('submit', getUser);
+// userForm.addEventListener('submit', getUser);
 
 function getUser(e) {
-    if (e.type === 'submit') e.preventDefault();
-    if (e.target.value) {
-        JSON.stringify(localStorage.setItem('user', e.target.value));
-        return;
+
+    const {value} = e.target;
+    let isMarc = '';
+    if (value === 'Marc') {
+        isMarc = 'the god himself blesses us with his presence';
     }
-    const getVal = userForm.querySelector('input').value;
-    JSON.stringify(localStorage.setItem('user', getVal));
+    const welcomeText = `Welcome ${value} ${isMarc}`;
+    status.textContent = welcomeText;
+    JSON.stringify(localStorage.setItem('user', value));
 }
 const name = localStorage.getItem('user');
 if (name) {
+    let isMarc = '';
+    if (name === 'Marc') isMarc = 'the god himself blesses us with his presence';
     userForm.querySelector('input').value = name;
-    status.textContent = `Welcome ${name}.`;
+    status.textContent = `Welcome ${name} ${isMarc}`;
     userForm.querySelector('label').textContent = 'Username already entered! You can still change it.'
 }
 
@@ -40,7 +44,7 @@ if (s !== statusDefault) {
 }
 });
 
-const socket = io.connect('/');
+const socket = io.connect('http://localhost:3000');
 
 if(socket) {
 socket.on('output', (data) => {
@@ -51,10 +55,9 @@ socket.on('output', (data) => {
             comment.innerHTML = `<strong>${item.name}:</strong> ${item.message}`;
             chat.classList.add('message');
             chat.appendChild(comment);
-            
+            scrollBottom();
        //     chat.insertBefore(comment, chat.firstChild);
         })
-        chat.scrollTo(0, 1000);
     }
 })
     socket.on('status', (data) => {
@@ -69,10 +72,14 @@ socket.on('output', (data) => {
                 name: username.value,
                 message: message.value
             });
-            chat.scrollTo({top: 1000, behavior: "smooth"});
             message.value = '';
+            scrollBottom();
             e.preventDefault();
     //    }
     })
 }
 })();
+
+function scrollBottom() {
+    chat.scrollTo(0, 1000);
+}
